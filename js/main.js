91956db1042ad4f2c86106ee77e7335e15257d64
juancarlosavalos <juan.carlos.avalos@outlook.com>
2020-05-26 16:28:52 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,96 @@
+//estas son las variables para suma ADDITION +
+
+var juanCarlos = 30
+var joseLuis = 25
+
+
+var additionResult = juanCarlos + joseLuis
+
+console.log("la suma de la edad de juanCarlos mas la edad de joseLuis", additionResult)
+document.write('<br/>');
+document.write("la suma de la edad de juancarlos mas la edad de joseLuis", juanCarlos + joseLuis)
+document.write('<br/>');
+
+//estas son las variables para resta SUBTRACTION -
+
+var anaMaria = 60
+var anaKaren = 20
+
+var substractionResult = anaMaria - anaKaren
+
+console.log("la resta de la edad de anaMaria menos la edad de anaKaren", substractionResult)
+document.write('<br/>');
+document.write("la resta de la edad de anaMaria menos la edad de anaKaren", anaMaria - anaKaren)
+document.write('<br/>');
+
+
+//estas son las variables para multiplicacion MULTIPLICATION *
+
+
+
+var multlipicationResult = juanCarlos * anaMaria * joseLuis * anaKaren
+
+
+console.log("la multiplicacion de las cuatro variabes anteriores ", multlipicationResult)
+document.write('<br/>');
+document.write("la multiplicacion de las cuatro variabes anteriores ", juanCarlos * anaMaria * joseLuis * anaKaren)
+document.write('<br/>');
+
+
+
+
+//estas son las variales para division DIVISION /
+
+
+var ageGrandma = 90
+var ageDaughter = 10
+
+var divicionResult = ageGrandma / ageDaughter
+
+console.log("Es el resultado de la divicion de la abula y la nieta ", divicionResult)
+document.write('<br/>');
+document.write("Es el resultado de la divicion de la abula y la nieta ", ageGrandma / ageDaughter)
+document.write('<br/>');
+
+
+
+
+//estas son las variales para modulo MODULUS (DIVISION REMAINDER) %
+
+
+var scoreAngel = 20.8
+var scoreRoberto = 90.1
+
+
+var modulusResult = scoreAngel % scoreRoberto
+
+console.log("este es el resultado de la puntuacion de dos jugadores ", modulusResult)
+document.write('<br/>');
+document.write("este es el resultado de la puntuacion de dos jugadores ", scoreAngel % scoreRoberto)
+document.write('<br/>');
+
+
+//estas son las variales para incremento INCREMENT ++
+
+
+
+var cashCarlos = 60
+var pageCash = ++cashCarlos
+
+console.log("este es el resultado del incremento de dinero de carlos", pageCash)
+document.write('<br/>');
+document.write("este es el resultado del incremento de ",cashCarlos++ )
+document.write('<br/>');
+
+
+
+//estas son las variales para decremento DECREMENT --
+
+
+var cashFabian = 60
+var pageCash = --cashFabian
+
+console.log("este es el resultado del incremento de dinero de carlos", pageCash)
+document.write('<br/>');
+document.write("este es el resultado del incremento de ",cashFabian-- )
+document.write('<br/>');
